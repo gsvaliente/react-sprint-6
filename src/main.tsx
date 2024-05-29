7c55fe9b0368f2fbe21c14551652aeccf5a3a4b1
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-05-29 11:33:04 +0200

    Added Welcome screen and separated routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import { AppProvider } from './context/AppContext.tsx';
+
 import './index.css';
+import { Welcome } from './pages/Welcome.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Welcome />,
+  },
+  {
+    path: '/budget',
+    element: (
+      <AppProvider>
+        <App />
+      </AppProvider>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,8 @@
+export function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to the Budget App</h1>
+      <button>Enter</button>
+    </div>
+  );
+}
