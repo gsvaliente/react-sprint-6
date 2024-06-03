67f57c7613e6140212fb80160624791771aa4115
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-06-03 11:43:08 +0200

    Created a Total component, and reorganized the App component order

---
+++ src/components/Total.tsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+export function Total() {
+  const { total } = useContext(AppContext) || { total: 0 };
+  return (
+    <div>
+      <div className='stats shadow'>
+        <div className='stat'>
+          <div className='stat-title'>Total Price</div>
+          <div className='stat-value'>$ {total}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,26 +1,23 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AddingBudget } from '../components/AddingBudget';
 import { BudgetCard } from '../components/BudgetCard';
 import { CardList } from '../components/CardList';
-import { AppContext } from '../context/AppContext';
+import { Total } from '../components/Total';
 
 function App() {
   // console.log(budget);
-  const { total } = useContext(AppContext) || { total: 0 };
 
   return (
     <>
       <header>This will be the header</header>
-      <CardList />
-      <p>The total will be ${total}</p>
-
       <Link
         className='btn'
         to={'/'}
       >
         Home
       </Link>
+      <CardList />
+      <Total />
       <AddingBudget />
       <BudgetCard />
     </>
