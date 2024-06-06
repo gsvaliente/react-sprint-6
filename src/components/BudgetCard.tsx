62c6c194fc8506f18c9229b41b470282aef72b23
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-06-06 10:01:38 +0200

    Styled the BudgetCard component

--- src/components/BudgetCard.tsx
+++ src/components/BudgetCard.tsx
@@ -1,33 +1,47 @@
-import { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
-import { BudgetCardType } from '../data/data';
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { BudgetCardType } from "../data/data";
 
 export function BudgetCard() {
   const { budgetList } = useContext(AppContext) || {};
 
   return (
-    <div>
-      {/* //TODO maybe fix the idx to a uuid or something similar */}
+    <div className="mt-10">
+      {/* TODO: change the idx for something like uuid or similar */}
       {budgetList?.map((el: BudgetCardType, idx: number) => {
         const newObj = Object.entries(el.services);
         const filteredObj = newObj.filter((el) => +el[1] > 0);
-
         return (
           <div key={idx}>
-            <div>
-              <h3>{el.name}</h3>
-              <p>{el.email}</p>
-              <p>{el.telephone}</p>
+            <div className="flex justify-center">
+              <div className="card sm:w-full md:w-10/12 bg-base-100 shadow-xl mb-5">
+                <div className="card-body flex flex-row justify-between">
+                  {/* USER INFO */}
+                  <div>
+                    <h2 className="card-title uppercase text-2xl">{el.name}</h2>
+                    <p className="text-sm">{el.telephone}</p>
+                    <p className="text-sm">{el.email}</p>
+                  </div>
+                  {/* SERVICES */}
+                  <div>
+                    <div className="">
+                      <p>
+                        <span>Contracted Services</span>
+                      </p>
+                      {filteredObj.map((service) => (
+                        <li key={service[0]}>{service[0]}</li>
+                      ))}
+                    </div>
+                  </div>
+                  {/* TOTAL */}
+                  <div>
+                    <h1 className="font-extrabold text-xl w-16 md:w-64  md:text-3xl">
+                      € {el.total}
+                    </h1>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div>
-              <p>
-                <span>Contracted Services</span>
-              </p>
-              {filteredObj.map((service) => (
-                <li key={service[0]}>{service[0]}</li>
-              ))}
-            </div>
-            {el.total}
           </div>
         );
       })}
